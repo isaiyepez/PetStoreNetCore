995d73cd6d41a142a352b3eac017c0b51c117c5d
Isai Yepez <isaiyepez@gmail.com>
2022-07-03 20:55:51 -0500

    Add project files.

---
+++ PetStoreNetCore.Core/Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetStoreNetCore.Core
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string StoreName { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+        public int CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
